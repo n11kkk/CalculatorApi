46825401c3a37170cb38ee944b09eb87a739928c
Nikilesh <nxsambamurthy@shockers.wichita.edu>
2022-11-03 23:10:07 -0500

    adding the api model

---
+++ Exercise.cs
@@ -0,0 +1,14 @@
+﻿namespace CalApi
+{
+    public class Exercise
+    {
+        public int[]? FirstNum { get; set; }
+        public int[]? SecondNum { get; set; }
+
+        public string[]? Operators { get; set; }
+        public int[]? Answers { get; set; }
+
+        public int[][]? Options { get; set; }
+
+    }
+}
